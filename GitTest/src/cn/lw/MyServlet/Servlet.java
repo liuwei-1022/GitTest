c3b39df7b7e45fded7d5260c3705603952c952ca
liuwei <252854080@qq.com>
2020-09-13 23:40:41 +0800

    3rd commit

--- GitTest/src/cn/lw/MyServlet/Servlet.java
+++ GitTest/src/cn/lw/MyServlet/Servlet.java
@@ -11,10 +11,19 @@ import java.io.IOException;
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("hhhhhh");
+
+        System.out.println("hhhhhh");
+        System.out.println("hhhhhh");
+        System.out.println("hhhhhh");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("hhhhhh");
+
+        System.out.println("hhhhhh");
+        System.out.println("hhhhhh");
         System.out.println("hhhhhh");
 
     }
